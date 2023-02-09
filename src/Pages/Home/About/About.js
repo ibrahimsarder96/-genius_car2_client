284e06aac6d1540df6453a173ad9754135e57cd0
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-02-09 23:58:14 +0600

    about page text add

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -4,11 +4,17 @@ const About = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row">
+    <div className='w-1/2'>
     <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-    <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+    </div>
+    <div className='w-1/2'>
+      <p className="text-xl font-bold text-orange-600">About Us</p>
+      <h1 className="text-5xl font-bold">We are qualified <br /> 
+      & of experience <br /> 
+      in this field</h1>
+      <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+      <p className="py-6">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+      <button className="btn btn-error text-white">Get More Info</button>
     </div>
   </div>
 </div>
